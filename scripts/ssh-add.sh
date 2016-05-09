428106da31bc42a01f348e75a20e910f88c30c41
Andrei Cioara <andrei@cioara.me>
2016-05-09 16:58:11 +0100

    added todo for sshadd and permission script

--- scripts/ssh-add.sh
+++ scripts/ssh-add.sh
@@ -2,6 +2,8 @@
 
 # ls ~/.ssh/*.pem | xargs -n 1 . ssh-add
 
+# TODO: This is still buggy
+
 # SSH - priority
 ssh-add ~/.ssh/identity.pem > /dev/null 2>&1
 
---
+++ scripts2/permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo chmod 755 $(sudo find /path/to/base/dir -type d)
+sudo chmod 644 $(sudo find /path/to/base/dir -type f)
