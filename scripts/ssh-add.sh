ce3f1271b94613c8189f321d318907adc139628d
Andrei Cioara <andrei@cioara.me>
2016-05-09 13:18:41 +0100

    fixed the ssh-add script

--- scripts/ssh-add.sh
+++ scripts/ssh-add.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-ls ~/.ssh/*.pem | xargs -n 1 ssh-add
+# ls ~/.ssh/*.pem | xargs -n 1 . ssh-add
 
+# SSH - priority
+ssh-add ~/.ssh/identity.pem > /dev/null 2>&1
+
+keys=$(ls ~/.ssh/*.pem)
+for key in $keys; do
+    ssh-add $key > /dev/null 2>&1
+done
+
+
+echo "Added SSH identities"
