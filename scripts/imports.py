f8f9a6824993c55429ecaf723836c1b8fcca2cd9
Andrei Cioara <andrei@cioara.me>
2017-11-05 03:07:23 +0000

    checkpoint before the great rebinding

--- scripts/imports.py
+++ scripts/imports.py
@@ -4,10 +4,9 @@ from __future__ import division
 
 import os
 import sys
-import numpy as np
 from math import *
 printf = sys.stdout.write
 from pprint import pprint
 import json
 import re
-
+import numpy as np
